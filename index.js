a830fe69d81f6628b69ebd4a6338f7ef5e0ff1ed
Roman Pylyponiuk <r.pylyponiuk@bvblogic.com>
2022-11-11 15:44:13 +0200

    test moment

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log('This is test git hub repo');
+
+const moment = require('moment'); // require
+
+const a = moment('2022-01-01');
+const b = a.add(1, 'year');
+a.format();
+
+console.log(b.format());
