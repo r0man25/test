5a37802a472f58aeeef45b4c2ac71ea21784a3ee
Roman Pylyponiuk <r.pylyponiuk@bvblogic.com>
2022-11-11 19:05:42 +0200

    edit 3

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ console.log('This is test git hub repo');
 const moment = require('moment'); // require
 
 const a = moment('2022-01-01');
-const b = a.add(2, 'year');
+const b = a.add(3, 'year');
 a.format();
 
 console.log(b.format());
